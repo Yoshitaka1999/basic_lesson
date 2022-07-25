006f7a987bfe03fdfe2ebe050acea8f8f425cdb2
加藤義隆 <991024yoshi@gmail.com>
2022-07-25 15:43:51 +0900

    CleanUp関数

---
+++ src/CleanUp.tsx
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from 'react'
+
+const CleanUp: React.FC = () => {
+    const [currentNum, setCurrentNum] = useState(0);
+    const incrementNum = () => {
+        console.log("Mouse event");
+        setCurrentNum((preNumber) => preNumber + 1);
+    }
+
+    useEffect(() => {
+        console.log("useEffect");
+        window.addEventListener("mousedown", incrementNum);
+        return () => {
+            console.log("Clean Up");
+            window.removeEventListener("mousedown", incrementNum);
+        }
+    },[])
+  return (
+    <div>
+      {currentNum}
+    </div>
+  )
+}
+
+export default CleanUp
